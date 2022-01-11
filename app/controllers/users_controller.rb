4db64cd2be20a69b8fa49de1bb4920e767d09daa
Hammad563 <munirhammad786@gmail.com>
2022-01-11 16:15:32 -0500

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+    before_action :authorized, only: [:auto_login]
+
+
+    # Register
+    def create 
+       @user = User.create(user_params)
+       if @user.valid?
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+       else
+            render json: {error: "Invalid username or password"}
+       end
+    end
+
+    # Log In
+    def login
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Invalid username or password"}
+        end
+    end
+
+    # auto_login
+
+    def auto_login
+        render json: @user
+    end
+
+    # user_params
+    private
+
+    def user_params
+        params.permit(:username, :password, :age)
+    end
+
+end
